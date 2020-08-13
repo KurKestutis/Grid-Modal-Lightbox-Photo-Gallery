9dbef28f839f6136b48d7b4d7c14b7dbb4b32240
Kestutis Kurienius <kurkestutis@gmail.com>
2020-08-13 16:51:23 +0300

    Button changed to photo (2)

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-var btn = document.getElementById('myBtn');
+var btn = document.getElementById('eventsGrid');
 
 // Get the <span> element that close the modal
 var span = document.getElementsByClassName('close')[0];
