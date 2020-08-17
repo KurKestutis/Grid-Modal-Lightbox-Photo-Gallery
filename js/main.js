905fb2a670fd3c361a8246c122393ad495b5eb71
Kestutis Kurienius <kurkestutis@gmail.com>
2020-08-17 09:38:33 +0300

    Minor changes, code cleaned second pic added

--- js/main.js
+++ js/main.js
@@ -1,21 +1,12 @@
-// Get the modal
-// var modal = document.getElementById('myModal');
+// When the user clicks on the button, open the modal
 function openModal() {
   document.getElementById('myModal').style.display = 'block';
 }
 
-// Get the button that opens the modal
-// var btn = document.getElementById('eventsGrid');
-
-// When the user clicks on the button, open the modal
-// btn.onclick = function () {
-//   modal.style.display = 'block';
-// };
-
-// MODAL CLOSE FUNCTIONS
+// ------------MODAL CLOSE FUNCTIONS--------------
 // Get the <span> element that close the modal
 var span = document.getElementsByClassName('close')[0];
-// When the user clicks on <span> (x), close tje modal
+// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   var modal = document.getElementById('myModal');
   modal.style.display = 'none';
